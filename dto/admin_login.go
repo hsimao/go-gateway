538717c232c377d1cdf6df387d4d1d61847f099f
hsimao <e087754958@gmail.com>
2020-09-11 20:53:49 +0800

    add: usernamd custom validation

--- dto/admin_login.go
+++ dto/admin_login.go
@@ -1,12 +1,12 @@
 package dto
 
 import (
-	"github.com/hsimao/go_gateway_demo/public"
 	"github.com/gin-gonic/gin"
+	"github.com/hsimao/go_gateway_demo/public"
 )
 
 type AdminLoginInput struct {
-	UserName string `json:"username" form:"username" comment:"姓名" example:"admin" validate:"required"`
+	UserName string `json:"username" form:"username" comment:"姓名" example:"admin" validate:"required,is_valid_username"`
 	Password string `json:"password" form:"password" comment:"密碼" example:"123456" validate:"required"`
 }
 
