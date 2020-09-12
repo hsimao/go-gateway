ac13792a0f8707da0b0e41cd0b09385b2f34b9a8
hsimao <e087754958@gmail.com>
2020-09-12 16:00:43 +0800

    add: update gitignore

--- controller/admin_login.go
+++ controller/admin_login.go
@@ -1,9 +1,9 @@
 package controller
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/hsimao/go_gateway_demo/dto"
 	"github.com/hsimao/go_gateway_demo/middleware"
-	"github.com/gin-gonic/gin"
 )
 
 type AdminLoginController struct{}
@@ -13,11 +13,22 @@ func AdminLoginRegister(group *gin.RouterGroup) {
 	group.POST("/login", adminLogin.AdminLogin)
 }
 
+// AdminLogin godoc
+// @Summary 管理員登入
+// @Description 管理員登入
+// @Tags 管理員接口
+// @ID /admin_login/login
+// @Accept  json
+// @Produce  json
+// @Param body body dto.AdminLoginInput true "body"
+// @Success 200 {object} middleware.Response{data=dto.AdminLoginOupt} "success"
+// @Router /admin_login/login [post]
 func (adminlogin *AdminLoginController) AdminLogin(c *gin.Context) {
 	params := &dto.AdminLoginInput{}
 	if err := params.BindValidParam(c); err != nil {
 		middleware.ResponseError(c, 1001, err)
 		return
 	}
-	middleware.ResponseSuccess(c, "")
+	out := &dto.AdminLoginOupt{Token: params.UserName}
+	middleware.ResponseSuccess(c, out)
 }
--- dto/admin_login.go
+++ dto/admin_login.go
@@ -6,10 +6,14 @@ import (
 )
 
 type AdminLoginInput struct {
-	UserName string `json:"username" form:"username" comment:"姓名" example:"admin" validate:"required,is_valid_username"`
-	Password string `json:"password" form:"password" comment:"密碼" example:"123456" validate:"required"`
+	UserName string `json:"username" form:"username" comment:"姓名" example:"admin" validate:"required,is_valid_username"` // 姓名
+	Password string `json:"password" form:"password" comment:"密碼" example:"123456" validate:"required"`                  // 密碼
 }
 
 func (param *AdminLoginInput) BindValidParam(c *gin.Context) error {
 	return public.DefaultGetValidParams(c, param)
+}
+
+type AdminLoginOupt struct {
+	Token string `json:"token" form:"token" comment:"token" example:"token" validate:""`
 }
